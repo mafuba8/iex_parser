80f0cf1fd5f98be06fe8d9a86fb341fa58397e2c
Benedikt Otto <b.otto.code@protonmail.com>
2025-08-30 15:10:01 +0200

    adjusted batch processing for multiple feeds.

--- batch_parse.py
+++ batch_parse.py
@@ -21,11 +21,11 @@ from multiprocessing import Pool
 NUM_OF_PROCESSES = 4
 
 def parse_file(file_tuple):
-    input_file, output_dir = file_tuple
+    input_file, output_dir, feed_type = file_tuple
     time_start = time.time()
 
     # Create the parser object and parse the files.
-    decoder = iex_parser.Decoder('DEEP_1_0')
+    decoder = iex_parser.Decoder(feed_type)
     parser = iex_parser.IEXFileParser(input_file, output_dir, decoder)
     parser.parse()
 
@@ -44,15 +44,25 @@ if __name__ == '__main__':
 
     # Build list of files to be parsed.
     input_file_list = []
-    regex_deep = re.compile(r'^data_feeds_(\d{8})_(\d{8})_IEXTP1_DEEP1\.0\.pcap\.gz$')
+    regex_deep_10 = re.compile(r'^data_feeds_(\d{8})_(\d{8})_IEXTP1_DEEP1\.0\.pcap\.gz$')
+    regex_tops_16 = re.compile(r'^data_feeds_(\d{8})_(\d{8})_IEXTP1_TOPS1\.6\.pcap\.gz$')
     for file in DIR_INPUT.iterdir():
         os.path.basename(file)
-        s = regex_deep.search(file.name)
-        if s:
-            date_stamp = s.group(1)
+        s_deep_10 = regex_deep_10.search(file.name)
+        s_tops_16 = regex_tops_16.search(file.name)
+
+        if s_deep_10:
+            date_stamp = s_deep_10.group(1)
             out_dir = (DIR_OUTPUT / 'DEEP1.0' / date_stamp)
-            t = (file, out_dir)
-            input_file_list.append(t)
+            out_dir.mkdir(parents=True, exist_ok=True)
+            arg_tuple = (file, out_dir, 'DEEP_1_0')
+            input_file_list.append(arg_tuple)
+        elif s_tops_16:
+            date_stamp = s_tops_16.group(1)
+            out_dir = (DIR_OUTPUT / 'TOPS1.6' / date_stamp)
+            out_dir.mkdir(parents=True, exist_ok=True)
+            arg_tuple = (file, out_dir, 'TOPS_1_6')
+            input_file_list.append(arg_tuple)
 
     # Parse the files in parallel.
     with Pool(processes=NUM_OF_PROCESSES) as pool:
